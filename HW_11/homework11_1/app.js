bf9f29fff14b0aada0198c4075409e9009e29e23
faisol-chehumar <30441151+faisol-chehumar@users.noreply.github.com>
2018-06-01 23:12:39 +0700

    Update HW11_1

--- HW_11/homework11_1/app.js
+++ HW_11/homework11_1/app.js
@@ -1,16 +1,40 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
+const Koa = require('koa')
+const Router = require('koa-router')
+const render = require('koa-ejs')
+const path = require('path')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'secret',
-  database: 'sqldb'
+const app = new Koa()
+const router = new Router()
+
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'secret',
+  database        : 'codecamp'
 })
 
-connection.query(
-  'SELECT * FROM person',
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'template',
+  viewExt: 'ejs',
+  cach: false
+})
+
+router.get('/', async (ctx) => {
+  const result = await pool.query('SELECT * FROM users;')
+  const data = {
+    key: Object.keys(result[0][0]),
+    rows: result[0]
   }
-)
+  console.log(data.rows)
+  await ctx.render('index', data)
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000)
