2b18edc63d7363bffbe86b40b2fe793e9d21a94f
faisol-chehumar <30441151+faisol-chehumar@users.noreply.github.com>
2018-06-02 09:47:20 +0700

    HW11_2

---
+++ HW_11/homework11_2/app.js
@@ -0,0 +1,63 @@
+const mysql = require('mysql2/promise')
+const Koa = require('koa')
+const Router = require('koa-router')
+const {promisify} = require('util')
+const fs = require('fs')
+const readFile = promisify(fs.readFile)
+
+
+const app = new Koa()
+const router = new Router()
+
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'secret',
+  database        : 'codecamp'
+})
+
+router.get('/', async (ctx, next) => {
+  ctx.body = 'Hello Dolores!'
+})
+
+router.get('/from_database', async (ctx, next) => {
+  const result = await pool.query('SELECT * FROM users;')
+  ctx.result = result[0] 
+  await next()
+})
+
+router.get('/from_file', async (ctx, next) => {
+  let dataJson = await readFile('./data/data.json')
+  ctx.result = JSON.parse(dataJson)
+  await next() 
+})
+
+async function setAdditionData(ctx, next) {
+  let today = new Date()
+  let year = today.getFullYear()
+  let month = today.getMonth() < 10 ? '0' + today.getMonth() : today.getMonth()
+  let date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate()
+  let hours = today.getHours() < 10 ? '0' + today.getHours() : today.getHours()
+  let minutes = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes()
+  let seconds = today.getSeconds()
+
+  let dateTime = `${year}-${month}-${date} ${hours}-${minutes}-${seconds}`
+
+  let additionData = {
+    data: ctx.result,
+    additionalData: {
+      userId: 1,
+      dateTime: dateTime
+    }
+  }
+  ctx.body = additionData
+  await next()
+}
+
+app.use(router.routes())
+app.use(setAdditionData)
+app.use(router.allowedMethods())
+
+app.listen(3000)
